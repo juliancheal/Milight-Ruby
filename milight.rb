a97baa224dab3a868c6c497a19039c0a4c032ff5
Julian Cheal <julian.cheal@gmail.com>
2014-08-21 17:38:59 +0300

    Added basic Milight class

---
+++ milight.rb
@@ -0,0 +1,23 @@
+# api http://www.limitlessled.com/dev/
+class Milight
+  
+  def initialize(args)
+    address
+    port # default 8899
+  end
+  
+  def lights_on
+  end
+  
+  def lights_off
+  end
+  
+  def dim
+  end
+  
+  def bright
+  end
+  
+  def light_status
+  end
+end
